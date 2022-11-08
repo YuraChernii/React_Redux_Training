590867059b26b44713d86b4660ec85d0b1411a42
Yurii Bartish <bartish@marketinglabs.ai>
2022-11-08 07:06:40 -0800

    add delete functionality

---
+++ src/reducers/index.js
@@ -0,0 +1,39 @@
+const initialState = {
+  heroes: [],
+  heroesLoadingStatus: "idle",
+  filters: [],
+};
+
+const reducer = (state = initialState, action) => {
+  debugger;
+  switch (action.type) {
+    case "HEROES_FETCHING":
+      return {
+        ...state,
+        heroesLoadingStatus: "loading",
+      };
+    case "HEROES_FETCHED":
+      return {
+        ...state,
+        heroes: action.payload,
+        heroesLoadingStatus: "idle",
+      };
+    case "HEROES_FETCHING_ERROR":
+      return {
+        ...state,
+        heroesLoadingStatus: "error",
+      };
+    case "HEROES_DELETED":
+      state.heroes = state.heroes.filter(
+        (item) => !action.payload.some((id) => id === item.id)
+      );
+      return {
+        ...state,
+        heroesLoadingStatus: "idle",
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
